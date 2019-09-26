9bc7b14e8717b40368851eac5a0a7011dbf23510
Ivan Lopatin <gingray.dev@gmail.com>
2019-09-26 16:31:21 +0200

    fix spec

--- spec/rulut_spec.rb
+++ spec/rulut_spec.rb
@@ -1,12 +1,12 @@
 RSpec.describe Rulut, focus: true do
   describe 'Rulut::Cube' do
     # let(:input) { "# some comments 1231\n# some other comments" }
-    # let(:input) { file_fixture('cube/small.cube').read }
-    let(:input) { file_fixture('cube/cinematic.cube').read }
+    let(:input) { file_fixture('cube/small.cube').read }
+    # let(:input) { file_fixture('cube/cinematic.cube').read }
     let(:result) { Rulut::Parser.parse_cube(input) }
     it do
       expect(result).to be_kind_of ::Treetop::Runtime::SyntaxNode
-      expect(result.to_object).to eq ''
+      expect(result.to_object).to be_a  Rulut::Cube
     end
   end
 end
